5b09a56edc8704372f5bbe09931c0eab8d6cb2d8
decvw(u23657597) <u23657597@tuks.co.za>
2025-05-12 20:13:01 +0200

    Step 3 done

--- ProductList/Models/ProductRepository.cs
+++ ProductList/Models/ProductRepository.cs
@@ -4,6 +4,16 @@ namespace ProductList.Models
     {
     public class ProductRepository
         {
+        public static List<Supplier> GetSuppliers()
+        {
+            return new List<Supplier>
+    {
+        new Supplier { Id = 1, Name = "Supplier 1" },
+        new Supplier { Id = 2, Name = "Supplier 2" },
+        new Supplier { Id = 3, Name = "Supplier 3" },        
+    };
+        }
+
         public static List<Category> GetCategories()
             {
             return new List<Category>
@@ -13,6 +23,10 @@ namespace ProductList.Models
         new Category { Id = 3, Name = "Category 3" },
         new Category { Id = 4, Name = "Category 4" },
         new Category { Id = 5, Name = "Category 5" },
+        new Category { Id = 6, Name = "Category 6" },
+        new Category { Id = 7, Name = "Category 7" },
+        new Category { Id = 8, Name = "Category 8" },
+        new Category { Id = 9, Name = "Category 9" },
     };
             }
 
@@ -29,8 +43,12 @@ namespace ProductList.Models
         new Product { Id = 7, Name = "Product 7", CategoryId = 4 },
         new Product { Id = 8, Name = "Product 8", CategoryId = 4 },
         new Product { Id = 9, Name = "Product 9", CategoryId = 5 },
-        new Product { Id = 10, Name = "Product 10", CategoryId = 5 },
-        new Product { Id = 11, Name = "Product 11", CategoryId = 5 },
+        new Product { Id = 10, Name = "Product 10", CategoryId = 6 },
+        new Product { Id = 11, Name = "Product 11", CategoryId = 6 },
+        new Product { Id = 12, Name = "Product 12", CategoryId = 7 },
+        new Product { Id = 13, Name = "Product 13", CategoryId = 8 },
+        new Product { Id = 14, Name = "Product 14", CategoryId = 8 },
+        new Product { Id = 15, Name = "Product 15", CategoryId = 9 },
     };
             }
         }
--- ProductList/Models/ProductViewModel.cs
+++ ProductList/Models/ProductViewModel.cs
@@ -8,5 +8,7 @@ namespace ProductList.Models
         public int SelectedProductId { get; set; }
         public List<Category> Categories { get; set; }
         public List<Product> Products { get; set; }
-        }
+        public List<Supplier> Suppliers { get; set; }
+        public int Serial { get; set; }
+    }
     }
